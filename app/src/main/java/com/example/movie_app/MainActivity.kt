a39ea49cecfb5e55cfa726c0e55495d5d5ca5a5f
AbdelrahmanAhmad313 <abdelrahmana.latif.11@gmail.com>
2025-09-22 17:46:49 +0300

    Done

---
+++ app/src/main/java/com/example/movie_app/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.movie_app
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.movie_app.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var b:ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        b =ActivityMainBinding.inflate(layoutInflater)
+        setContentView(b.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+    }
+}
