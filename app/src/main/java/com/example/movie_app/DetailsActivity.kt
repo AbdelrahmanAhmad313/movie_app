1692b3f337c8bfa9f5e74c8770ad50ac8ead0940
AbdelrahmanAhmad313 <abdelrahmana.latif.11@gmail.com>
2025-09-27 18:54:25 +0300

    Done

--- app/src/main/java/com/example/movie_app/DetailsActivity.kt
+++ app/src/main/java/com/example/movie_app/DetailsActivity.kt
@@ -5,6 +5,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.example.movie_app.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
@@ -12,13 +14,32 @@ class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        val bd = ActivityDetailsBinding.inflate(layoutInflater)
-        setContentView(bd.root)
+        val b = ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(b.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
+        val backG= b.background
+        val pstr =b.posterInside
+        val title=b.movieName
+        val calender=b.date
+        val background =intent.getStringExtra("background")
+        val poster =intent.getStringExtra("poster")
+        val name =intent.getStringExtra("title")
+        val date =intent.getStringExtra("date")
+        Glide.with(this)
+            .load(background)
+            .error(R.drawable.baseline_image_24)
+            .transition(DrawableTransitionOptions.withCrossFade(1000))
+            .into(b.background)
+        Glide.with(this)
+            .load(poster)
+            .error(R.drawable.baseline_image_24)
+            .transition(DrawableTransitionOptions.withCrossFade(1000))
+            .into(b.posterInside)
+            title.text=name
+            calender.text=date
     }
 }
